d01bf64b896f964b4446acbd11a047e8efbd22ef
Caliph Dev <92326690+caliphdev@users.noreply.github.com>
2023-08-22 20:16:33 +0700

    Update Constants.js

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -7,7 +7,7 @@ exports.DefaultOptions = {
         headless: true,
         defaultViewport: null
     },
-    webVersion: '2.2322.15',
+    webVersion: '2.2334.12',
     webVersionCache: {
         type: 'local',
     },
